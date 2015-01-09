8b80299f7830e43071dc409481e5f7a601293414
Robb C. Bennett <Visual23@Robbs-Mac-Pro.local>
2015-01-09 10:50:27 -0500

    first commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,9 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Robb C. Bennett on 1/7/15.
+//  Copyright (c) 2015 Visual23. All rights reserved.
+//
+
+import Foundation
---
+++ SlotMachine/SlotBrain.swift
@@ -0,0 +1,9 @@
+//
+//  SlotBrain.swift
+//  SlotMachine
+//
+//  Created by Robb C. Bennett on 1/8/15.
+//  Copyright (c) 2015 Visual23. All rights reserved.
+//
+
+import Foundation
